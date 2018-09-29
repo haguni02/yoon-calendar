a181bf4275f2ebc9254d5756168b8e2961ff96a9
Hakyoon <Hakyoon@Hakyoon-PC>
2018-09-29 17:03:29 +0900

    Add to method isLeapYear

--- src/project/calender/Calendar.java
+++ src/project/calender/Calendar.java
@@ -3,9 +3,23 @@ package project.calender;
 public class Calendar {
 
 	private static final int[] END_OF_MONTH = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_YEAR_END_OF_MONTH = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getEndOfMonth(int month) {
-		return END_OF_MONTH[month - 1];
+	public boolean isLeapYear(int year) {
+		if((year%4)==0 && ((year%100)!=0 || (year%400)==0)) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	public int getEndOfMonth(boolean leapYear, int month) {
+		if(leapYear) {
+			return LEAP_YEAR_END_OF_MONTH[month - 1];
+		} else {
+			return END_OF_MONTH[month - 1];
+		}
 	}
 
 	public void printCalendar(int year, int month) {
@@ -14,7 +28,8 @@ public class Calendar {
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("------------------------");
 		
-		int endOfMonth = getEndOfMonth(month);
+		boolean leapYear = isLeapYear(year);
+		int endOfMonth = getEndOfMonth(leapYear, month);
 		
 		for(int i=1; i<=endOfMonth; i++) {
 			System.out.printf("%3d",i);
--- src/project/calender/Prompt.java
+++ src/project/calender/Prompt.java
@@ -4,30 +4,37 @@ import java.util.Scanner;
 
 public class Prompt {
 
-	private static final String PROMPT = "cal>";
+//	private static final String PROMPT = "cal>";
 
 	public void runPrompt() {
 
 		// 입력받은 월의 최대 일수 출력하기
 		int month; // 달 입력
+		int year; // 년도 입력
 		Calendar cal = new Calendar();
 		Scanner scanner = new Scanner(System.in);
 
 		month = 1;
 
 		while (true) {
-			System.out.println("달을 입력하세요.(-1:종료)");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			if (month == -1)
+			System.out.println("년도를 입력하세요.(-1:종료)");
+			System.out.print("YEAR>");
+			year = scanner.nextInt();
+			
+			if (year == -1)
 				break;
+			
+			System.out.println("달을 입력하세요.");
+			System.out.print("MONTH>");
+			month = scanner.nextInt();
+			
 
 			if (month > 12 || month < 1) {
 				System.out.println("달은 1월부터 12월까지 입력하실 수 있습니다.");
 				continue;
 			}
 
-			cal.printCalendar(2018, month);
+			cal.printCalendar(year, month);
 		}
 		System.out.println("Have a nice day");
 
