3fe67f91a548c3d79358bbd98e8b7792f8a43a9c
Hakyoon <Hakyoon@Hakyoon-PC>
2018-09-29 15:20:39 +0900

    Change get end of months to method

--- src/project/calender/Calendar.java
+++ src/project/calender/Calendar.java
@@ -3,9 +3,14 @@ package project.calender;
 import java.util.Scanner;
 
 public class Calendar {
-
-	public static void main(String[] args) {
-
+	
+	private static final int[] END_OF_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getEndOfMonth(int month) {
+		return END_OF_MONTH[month-1];
+	}
+	
+	public static void printSampleCalendar() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
 		System.out.println("1  2  3  4  5  6  7");
@@ -13,27 +18,23 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		
-		// 입력받은 월의 최대 일수 출력하기
+	}
+
+	public static void main(String[] args) {
+
 		
-		// 달마다 최대 일수 등록
-		int[] months = new int[12];
-		for(int i=0; i<months.length; i++) {
-			if (i==1) {
-				months[i] = 28;
-			} else if (i==3 || i==5 || i==8 || i==10) {
-				months[i] = 30;
-			} else {
-				months[i] = 31;
-			}
-		}
+		// 입력받은 월의 최대 일수 출력하기
+	
+		
 		// 입력 받기
 		int month;
+		Calendar cal = new Calendar();
 		System.out.println("달을 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
 		month = scanner.nextInt();
 		
 		// 출력 하기
-		System.out.printf("%d월은 %d일까지 있습니다.",month,months[month-1]);
+		System.out.printf("%d월은 %d일까지 있습니다.",month,cal.getEndOfMonth(month));
 		scanner.close();
 	}
 
