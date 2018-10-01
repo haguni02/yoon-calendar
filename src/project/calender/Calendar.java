23a82acf22b33267e8a7b8dd2bd8fad5551cb828
Hakyoon <Hakyoon@Hakyoon-PC>
2018-10-01 22:46:13 +0900

    Get automatic day of week

--- src/project/calender/Calendar.java
+++ src/project/calender/Calendar.java
@@ -21,8 +21,40 @@ public class Calendar {
 			return END_OF_MONTH[month - 1];
 		}
 	}
+	
+	public int getWeekDay(int year, int month) { // 요일 알고리즘을 이용한 요일 구하기
+		
+		int standardYear = 1800; // 기준년도
+		int standardWeek = 3; // 1800년 1월 1일은 수요일
+		
+		int days = 0; // 입력한 날짜에서 기준년도의 월까지 지난 일수들
+		
+		int weekDay; // 구하는 요일 (0:sun~ 6:sat)
+		
+		for(int i=standardYear; i<year; i++) {
+			if(isLeapYear(i)) {
+				days += 366;
+			} else {
+				days += 365;
+			}
+		}
+		
+		for(int j=0; j<month-1; j++) {
+			if(isLeapYear(standardYear)) {
+				days += LEAP_YEAR_END_OF_MONTH[j];
+			} else {
+				days += END_OF_MONTH[j];
+			}
+		}
+		
+		weekDay = (standardWeek + (days%7)) %7;
+		
+		
+		return weekDay;
+	}
 
-	public void printCalendar(int year, int month, int week) {
+	public void printCalendar(int year, int month) {
+		
 
 		System.out.printf("   <<%4d년%3d월>> \n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
@@ -30,6 +62,8 @@ public class Calendar {
 		
 		boolean leapYear = isLeapYear(year);
 		int endOfMonth = getEndOfMonth(leapYear, month);
+		
+		int week = getWeekDay(year, month);
 		
 		
 		for(int j=0; j<week; j++) {
--- src/project/calender/Prompt.java
+++ src/project/calender/Prompt.java
@@ -5,43 +5,18 @@ import java.util.Scanner;
 public class Prompt {
 
 //	private static final String PROMPT = "cal>";
+	
+	private Scanner scanner = new Scanner(System.in);
 
-	public int parseWeekName(String week) {
-		switch (week) {
-		case "SU":
-		case "su":
-			return 0;
-		case "MO":
-		case "mo":
-			return 1;
-		case "TU":
-		case "tu":
-			return 2;
-		case "WE":
-		case "we":
-			return 3;
-		case "TH":
-		case "th":
-			return 4;
-		case "FR":
-		case "fr":
-			return 5;
-		case "SA":
-		case "sa":
-			return 6;
-		default:
-			return -1;
-		}
-	}
+
 
 	public void runPrompt() {
 
 		// 입력받은 월의 최대 일수 출력하기
 		int month; // 달 입력
 		int year; // 년도 입력
-		int week; // 요일 입력
 		Calendar cal = new Calendar();
-		Scanner scanner = new Scanner(System.in);
+		
 
 		month = 1;
 
@@ -57,22 +32,14 @@ public class Prompt {
 			System.out.print("MONTH>");
 			month = scanner.nextInt();
 
-			System.out.println("요일을 입력하세요.(SU, MO, TU, WE, TH, FR, SA)");
-			System.out.print("WEEK>");
-			String weekName = scanner.next();
-			week = parseWeekName(weekName);
-			
-			if (week == -1) {
-				System.out.println("요일을 잘 못 입력하셨습니다.");
-				continue;
-			}
 
 			if (month > 12 || month < 1) {
 				System.out.println("달은 1월부터 12월까지 입력하실 수 있습니다.");
 				continue;
 			}
+			
 
-			cal.printCalendar(year, month, week);
+			cal.printCalendar(year, month);
 		}
 		System.out.println("Have a nice day");
 
